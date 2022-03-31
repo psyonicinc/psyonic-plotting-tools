e1750b7c1930d6c9e6676d01c7c79fd9d1952357
ocanath <ocanath@gmail.com>
2022-03-31 16:52:19 -0500

    removed unnecessary fmod2pi from the calibration plotter and made path for script relative

--- scripts/windows/run_calibration_plot.bat
+++ scripts/windows/run_calibration_plot.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Running Calibration Lines Plotter
-python %~dp0\..\..\plot_lines.py -b 115200 -n 4 -w 50 -t 2000 --ymin -0.02 --ymax 0.08 --scaler cal
+python ..\..\plot_lines.py -b 230400 -n 4 -w 50 -t 2000 --ymin -0.02 --ymax 0.08 --scaler cal

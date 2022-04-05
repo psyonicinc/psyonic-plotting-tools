ad2f8c1547dd1517395c7cdd099c3a504c30fd25
ocanath <ocanath@gmail.com>
2022-04-05 13:46:19 -0500

    changed buffer setting on calibration plot for readability

--- scripts/windows/run_calibration_plot.bat
+++ scripts/windows/run_calibration_plot.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Running Calibration Lines Plotter
-python ..\..\plot_lines.py -b 230400 -n 4 -w 15 -t 6000 --ymin -0.01 --ymax 0.01 --scaler cal
+python ..\..\plot_lines.py -b 230400 -n 4 -w 7 --buffer 50 -t 6000 --ymin -0.01 --ymax 0.01 --scaler cal

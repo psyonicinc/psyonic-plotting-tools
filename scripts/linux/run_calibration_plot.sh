4960b8e19326da35c90eb12dfe5662169b28efd7
ocanath <ocanath@gmail.com>
2024-07-10 16:02:51 -0700

    new plot bounds

--- scripts/linux/run_calibration_plot.sh
+++ scripts/linux/run_calibration_plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Running Calibration Lines Plotter
-python3 $(dirname $(realpath $0))/../../plot_lines.py -b 230400 -n 4 -w 50 -t 2000 --ymin -40.96 --ymax 40.96 --scaler cal --passfail
+python3 $(dirname $(realpath $0))/../../plot_lines.py -b 230400 -n 4 -w 50 -t 2000 --ymin -163.84 --ymax 163.84 --scaler cal --passfail

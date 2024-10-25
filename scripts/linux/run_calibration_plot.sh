64abf456e2c2f5bad8c7bc75586827a27b601ba9
ocanath <ocanath@gmail.com>
2024-10-25 16:28:50 -0700

    cal verification plot params tweaking

--- scripts/linux/run_calibration_plot.sh
+++ scripts/linux/run_calibration_plot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo Running Calibration Lines Plotter
-python3 $(dirname $(realpath $0))/../../plot_lines.py -b 230400 -n 4 -w 50 -t 2000 --ymin -163.84 --ymax 163.84 --scaler cal --passfail
+python3 $(dirname $(realpath $0))/../../plot_lines.py -b 230400 -n 4 -w 50 -t 2000 --ymin -183.84 --ymax 183.84 --scaler cal --passfail

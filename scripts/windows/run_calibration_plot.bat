441abe740084ba9826d4c2980332f2387d43a6a6
PSYONIC <liuaimee003@gmail.com>
2024-12-17 19:29:38 -0800

    updated win calibration plot to match

--- scripts/windows/run_calibration_plot.bat
+++ scripts/windows/run_calibration_plot.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Running Calibration Lines Plotter
 SET fpath=%~dp0
-python  "%fpath:~0,-1%"\..\..\plot_lines.py -b 230400 -n 4 -w 50 -t 2000 --ymin -163.84 --ymax 163.84 --scaler cal --passfail
+python  "%fpath:~0,-1%"\..\..\plot_lines.py -b 921600 --buffer 300 -n 7 -w 300 -t 6000 --ymin -0.05 --ymax 100.5 --scaler peu
